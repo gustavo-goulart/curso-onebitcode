32f66e32ee499d0b8f4cc01d44c23c3ff5e7e865
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-04-02 14:09:08 -0300

    estruturas condicionais if e else

---
+++ javascript/javascript-1/aula16-if-else/script.js
@@ -0,0 +1,12 @@
+// const idade = prompt("Informe sua idade: ");
+
+// if (idade >= 18) {
+//   alert("Você é maior de idade");
+// } else if (idade > 12) {
+//   alert("Você é menor de idade");
+// } else if (idade > 4) {
+//   alert("Você é criança");
+// }
+
+const resultado = 6 === 5 ? "Verdadeiro" : false;
+console.log(resultado);
--- javascript/javascript-1/exercicios/exercicio-01/calculadora-4-operacoes/script.js
+++ javascript/javascript-1/exercicios/exercicio-01/calculadora-4-operacoes/script.js
@@ -3,15 +3,15 @@ Escreva um programa em javascript que permita inserir dois valores numéricos e 
 Após calcular os resultados o programa deve exibi-los na tela.
 */
 
-let numero1 = parseFloat(prompt("Digite um número: "));
-let numero2 = parseFloat(prompt("Digite outro número: "));
+const numero1 = parseFloat(prompt("Digite um número: "));
+const numero2 = parseFloat(prompt("Digite outro número: "));
 
-let soma = numero1 + numero2;
-let subtracao = numero1 - numero2;
-let multiplicacao = numero1 * numero2;
-let divisao = numero1 / numero2;
+const soma = numero1 + numero2;
+const subtracao = numero1 - numero2;
+const multiplicacao = numero1 * numero2;
+const divisao = numero1 / numero2;
 
 alert(`A soma entre ${numero1} e ${numero2} é: ${soma},
-A subtração entre ${numero1} e ${numero2} é: ${subtracao},
-A multiplicação entre ${numero1} e ${numero2} é: ${multiplicacao},
-A divisão entre ${numero1} e ${numero2} é: ${divisao}`);
+\nA subtração entre ${numero1} e ${numero2} é: ${subtracao},
+\nA multiplicação entre ${numero1} e ${numero2} é: ${multiplicacao},
+\nA divisão entre ${numero1} e ${numero2} é: ${divisao}`);
