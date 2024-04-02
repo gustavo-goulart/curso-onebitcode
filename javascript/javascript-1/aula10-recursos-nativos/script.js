e5d147bb9bc771d91e24dd43acdceefac4998abc
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-04-02 11:50:56 -0300

    recursos nativos do navegador

---
+++ javascript/javascript-1/aula10-recursos-nativos/script.js
@@ -0,0 +1,11 @@
+let nome = "Gustavo";
+
+console.log(nome);
+console.error(nome);
+console.warn(nome);
+
+// window.alert(nome);
+// nome = window.prompt("Insira o seu nome: ");
+let confirmacao = window.confirm("Confirma essa ação?");
+
+console.log(confirmacao);
