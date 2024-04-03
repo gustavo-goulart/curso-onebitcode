1952dc9efbc96dfbdb9ea8b321c730e3f3fe4959
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-04-03 15:24:56 -0300

    do while

---
+++ javascript/javascript-1/aula27-do-while/script.js
@@ -0,0 +1,8 @@
+let velocidade = 0;
+
+do {
+  alert("A velocidade do veículo é: " + velocidade + " km/h");
+  velocidade -= 20;
+} while (velocidade > 0);
+
+alert("Velocidade final: " + velocidade + " km/h");
