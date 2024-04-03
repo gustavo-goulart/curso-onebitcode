71908028c78a46cf7d5cd8e7c569d613398746db
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-04-03 14:33:08 -0300

    while

---
+++ javascript/javascript-1/aula24-while/script.js
@@ -0,0 +1,13 @@
+let velocidade = 80;
+
+while (velocidade > 0) {
+  alert("O carro está a " + velocidade + " km/h");
+  velocidade -= 20;
+  alert("Diminuindo 20 km/h");
+
+  if (velocidade === 40) {
+    break;
+  }
+}
+
+alert("O carro parou!!");
