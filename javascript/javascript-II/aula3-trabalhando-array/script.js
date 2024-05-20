4d84f5e64ea125912319cc4536960dc8b655dfec
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-05-20 16:22:57 -0300

    trabalhando com arrays

---
+++ javascript/javascript-II/aula3-trabalhando-array/script.js
@@ -0,0 +1,26 @@
+const arr = [
+  "Frodo",
+  "Sam",
+  "Merry",
+  "Pippin",
+  "Gandalf",
+  "Aragorn",
+  "Legolas",
+  "Gimli",
+];
+
+console.log(arr);
+
+// //adicionar elementos - push
+// // push: Adiciona um elemento no final do array e devolve o novo tamanho do array
+// arr.push("Boromir");
+// console.log(arr);
+
+// //unshift
+// arr.unshift("Gustavo");
+// console.log(arr);
+
+// pop: Remove um elemento no final do array e devolve o elemento removido
+let elementoRemovido = arr.pop("Boromir");
+console.log(arr);
+console.log(elementoRemovido);
