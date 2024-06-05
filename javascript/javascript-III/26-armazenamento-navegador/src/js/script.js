b46b3b745068958f4f4b03328099ba8949548704
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-06-05 18:51:08 -0300

    armazenamento web

---
+++ javascript/javascript-III/26-armazenamento-navegador/src/js/script.js
@@ -0,0 +1,46 @@
+//Usando o sessionStorage
+document.getElementById("sessionBtn").addEventListener("click", function () {
+  const input = document.getElementById("session");
+  sessionStorage.setItem("info", input.value);
+  input.value = "";
+});
+
+document.getElementById("readSesssion").addEventListener("click", function () {
+  const info = sessionStorage.getItem("info");
+  alert("A informação salva é: " + info);
+});
+
+//Usando o localStorage
+document.getElementById("localBtn").addEventListener("click", function () {
+  const input = document.getElementById("local");
+  localStorage.setItem("text", input.value);
+  input.value = "";
+});
+
+document.getElementById("readLocal").addEventListener("click", function () {
+  const t = localStorage.getItem("text");
+  alert("O texto salvo no local storage é: " + t);
+});
+
+//Usando o Cookies
+document.getElementById("cookieBtn").addEventListener("click", function () {
+  const input = document.getElementById("cookie");
+  // cookieName=value; expires=UTCStringDate; path=/;
+  const cookie = "info=" + input.value + ";";
+  const expiration = "expires=" + new Date(2024, 6, 6) + ";";
+  const path = "path=/;";
+  document.cookie = cookie + expiration + path;
+  input.value = "";
+  console.log(document.cookie);
+});
+
+document.getElementById("cookie2Btn").addEventListener("click", function () {
+  const input = document.getElementById("cookie2");
+  // cookieName=value; expires=UTCStringDate; path=/;
+  const cookie = "text=" + input.value + ";";
+  const expiration = "expires=" + new Date(2024, 7, 6) + ";";
+  const path = "path=/;";
+  document.cookie = cookie + expiration + path;
+  input.value = "";
+  console.log(document.cookie);
+});
