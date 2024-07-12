8541d0e137e48252f9041d44ff64d379cdeea07b
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-07-12 10:58:26 -0300

    ESMODULES

---
+++ javascript/javascript-IV/17-esmodules/index.js
@@ -0,0 +1,12 @@
+import { name, label, input, br } from "./functions.js";
+
+console.log(name);
+console.log(label({ for: "fullname", textContent: "Nome Completo" }));
+console.log(
+  input({
+    id: "fullname",
+    name: "fullname",
+    placeholder: "Digite seu nome completo...",
+  })
+);
+console.log(br());
